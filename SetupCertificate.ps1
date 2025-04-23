2454f6535e1157d218105ca9d7cdb3a59b972fcb
Henrik Helgesen <henrik@helgesen.consulting>
2025-04-23 02:20:52 -0700

    Update SetupCertificate.ps1

--- SetupCertificate.ps1
+++ SetupCertificate.ps1
@@ -23,5 +23,6 @@ Import-PfxCertificate -Password $SecurePfxPassword -FilePath $certificatePfxFile
 $dnsidentity = $cert.GetNameInfo('SimpleName',$false)
 
 Write-Host "Setting Tls12"
-Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name "SchUseStrongCrypto" -value 00000001 
+Set-ItemProperty -path "HKLM:\SOFTWARE\Microsoft\.NETFramework\v4.0.30319" -name "SchUseStrongCrypto" -value 00000001
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
 Write-Host "Tls12 Enabled"
